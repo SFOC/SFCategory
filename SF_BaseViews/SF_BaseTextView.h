f259e7b0c2a59b0c4d1ccb80066a1f61aa7548c9
fly <1005213565@qq.com>
2018-06-13 14:28:20 +0800

    添加了baseTextView

---
+++ SF_BaseViews/SF_BaseTextView.h
@@ -0,0 +1,43 @@
+//
+//  SF_BaseTextView.h
+//  YN_PassWordView
+//
+//  Created by fly（石峰） on 2018/6/13.
+//  Copyright © 2018年 ijianghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SF_BaseTextView : UITextView
+
+/// 限制字数设置（不设置则为无数）
+@property (nonatomic, assign) NSInteger limitWordsNum;
+
+/// 占位字符
+@property (nonatomic, strong) NSString *placeholder;
+
+/// 内边距设置(默认 5，5，5，5)
+@property (nonatomic, assign) UIEdgeInsets paddingEdgeInset;
+
+/// 边框颜色（默认为f2f2f2）
+@property (nonatomic, strong) UIColor *borderColor;
+
+/// 边框宽度（默认为1）
+@property (nonatomic, assign) CGFloat borderWidth;
+
+/// 是否有圆角（默认有）
+@property (nonatomic, assign) BOOL isRoundCorner;
+
+/// 圆角值 (默认5)
+@property (nonatomic, assign) CGFloat roundCornerValue;
+
+/// 字体大小（默认15）
+@property (nonatomic, assign) CGFloat fontSize;
+
+/// 光标颜色（默认blue）
+@property (nonatomic, strong) UIColor *cursorColor;
+#pragma mark ---颜色---
+/// text变化的时候的block回调
+@property (nonatomic, copy) void (^textChangeBlock)(NSString *text);
+
+@end
---
+++ SF_BaseViews/SF_BaseTextView.m
@@ -0,0 +1,228 @@
+//
+//  SF_BaseTextView.m
+//  YN_PassWordView
+//
+//  Created by fly（石峰） on 2018/6/13.
+//  Copyright © 2018年 ijianghu. All rights reserved.
+//
+
+#import "SF_BaseTextView.h"
+
+@interface SF_BaseTextView ()<UITextViewDelegate>
+
+/// 占位字符label
+@property (nonatomic, strong) UILabel *placeholderLabel;
+
+@end
+
+// RGBA_十六进制颜色处理
+#define ColorFromRGBAHEX(rgbaHex) [UIColor colorWithRed:((float)((rgbaHex & 0xFF0000) >> 16))/255.0 green:((float)((rgbaHex & 0xFF00) >> 8))/255.0 blue:((float)(rgbaHex & 0xFF))/255.0 alpha:1.0]
+@implementation SF_BaseTextView
+
+#pragma mark ---初始化---
+
+/// 直接init的时候先调用initWithFrame在调用init
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        [self initializeSetting];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        [self initializeSetting];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self layoutIfNeeded];
+    [self initializeSetting];
+}
+
+#pragma mark ---初始化设置---
+/// 初始化设置
+- (void) initializeSetting {
+    
+    self.limitWordsNum = -1;
+    self.isRoundCorner = YES;
+    self.paddingEdgeInset = UIEdgeInsetsMake(5, 5, 5, 5);
+    self.borderColor = ColorFromRGBAHEX(0xf2f2f2);
+    self.borderWidth = 1;
+    self.roundCornerValue = 5;
+    self.fontSize = 15;
+    /// 设置光标颜色
+    self.cursorColor = [UIColor blueColor];
+    
+    self.layer.masksToBounds = YES;
+    self.backgroundColor = [UIColor whiteColor];
+    self.textColor = ColorFromRGBAHEX(0x333333);
+    self.delegate = self;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textViewDidChangeAction:) name:UITextViewTextDidChangeNotification object:self];
+    
+    [self addObserver:self forKeyPath:@"font" options:NSKeyValueObservingOptionNew |
+     NSKeyValueObservingOptionOld context:nil];
+    [self addObserver:self forKeyPath:@"textContainerInset" options:NSKeyValueObservingOptionNew |
+     NSKeyValueObservingOptionOld context:nil];
+   
+    [self addSubview:self.placeholderLabel];
+}
+
+#pragma mark ---观察者方法---
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:@"font"]) {
+        
+        UITextView *textView = (UITextView *)object;
+        self.placeholderLabel.font = textView.font;
+        
+        self.placeholderLabel.frame = CGRectMake(self.paddingEdgeInset.left, self.paddingEdgeInset.top, self.frame.size.width - self.paddingEdgeInset.left - self.paddingEdgeInset.right, self.placeholderLabel.font.lineHeight);
+    }else if ([keyPath isEqualToString:@"textContainerInset"]) {
+        
+        UITextView *textView = (UITextView *)object;
+        UIEdgeInsets edgeInset = textView.textContainerInset;
+        self.placeholderLabel.frame = CGRectMake(edgeInset.left, edgeInset.top, self.frame.size.width - edgeInset.left - edgeInset.right, self.placeholderLabel.font.lineHeight);
+    }
+}
+
+#pragma mark ---set方法---
+- (void)setPaddingEdgeInset:(UIEdgeInsets)paddingEdgeInset {
+    _paddingEdgeInset = paddingEdgeInset;
+    
+    self.textContainerInset = paddingEdgeInset;
+    self.textContainer.lineFragmentPadding = 0;
+}
+
+- (void)setIsRoundCorner:(BOOL)isRoundCorner {
+    _isRoundCorner = isRoundCorner;
+    
+    if (isRoundCorner) {
+        
+        self.layer.cornerRadius = 0;
+    }else {
+        
+        self.layer.cornerRadius = self.roundCornerValue;
+    }
+}
+
+- (void)setBorderColor:(UIColor *)borderColor {
+    _borderColor = borderColor;
+    
+     self.layer.borderColor = borderColor.CGColor;
+}
+
+- (void)setBorderWidth:(CGFloat)borderWidth {
+    _borderWidth = borderWidth;
+    self.layer.borderWidth = borderWidth;
+}
+
+- (void)setRoundCornerValue:(CGFloat)roundCornerValue {
+    _roundCornerValue = roundCornerValue;
+    
+    self.layer.cornerRadius = roundCornerValue;
+}
+
+- (void)setPlaceholder:(NSString *)placeholder {
+    _placeholder = placeholder;
+    
+    self.placeholderLabel.text = placeholder;
+}
+
+- (void)setFontSize:(CGFloat)fontSize {
+    
+    _fontSize = fontSize;
+    
+    self.font = [UIFont systemFontOfSize:fontSize];
+}
+
+- (void)setCursorColor:(UIColor *)cursorColor {
+    _cursorColor = cursorColor;
+    
+    self.tintColor = cursorColor;
+}
+
+#pragma mark ---懒加载---
+- (UILabel *)placeholderLabel {
+    
+    if (!_placeholderLabel) {
+        
+        _placeholderLabel = [UILabel new];
+        _placeholderLabel.font = [UIFont systemFontOfSize:self.fontSize];
+        _placeholderLabel.frame = CGRectMake(self.paddingEdgeInset.left, self.paddingEdgeInset.top, self.frame.size.width - self.paddingEdgeInset.left - self.paddingEdgeInset.right, _placeholderLabel.font.lineHeight);
+        
+        _placeholderLabel.backgroundColor = [UIColor clearColor];
+        _placeholderLabel.textColor = ColorFromRGBAHEX(0xbbbbbb);
+    }
+    
+    return _placeholderLabel;
+}
+
+#pragma mark ---通知回调---
+- (void) textViewDidChangeAction:(NSNotification *)sender {
+    
+    if (self.text.length == 0) {
+        
+        self.placeholderLabel.hidden = NO;
+    }else {
+        
+        self.placeholderLabel.hidden = YES;
+    }
+    
+    if (self.textChangeBlock) {
+        
+        self.textChangeBlock(self.text);
+    }
+}
+#pragma mark ---UITextViewDelegate---
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    
+    if (self.limitWordsNum <= 0) {
+        
+        return YES;
+    }else if (text.length == 0) {
+        
+        return YES;
+    }else if (textView.text.length >= self.limitWordsNum) {
+        
+        return NO;
+    }
+    return YES;
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    
+    self.placeholderLabel.hidden = YES;
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
